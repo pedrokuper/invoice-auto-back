6cb3c5725d7de357978291795e7c335def3c0f39
Pedro Kupersmit <pkupersmit@gmail.com>
2021-11-22 03:35:02 -0300

    buffer destructuring

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -3,17 +3,22 @@ const XlsxPopulate = require("xlsx-populate");
 const regexMatcher = require("../utils/regexMatcher");
 const fs = require("fs");
 
+// TODO  Usar toda esta lógica del controller desde la función PDF extractor. Como estamos usando regex Matcher
 class InvoiceController {
   parsePDF(req, res) {
     const bodyData = JSON.parse(req.body.data);
+    const { buffer } = req.file;
 
-    if (req.file.buffer && bodyData.vendor && bodyData.email) {
+    if (buffer && bodyData.vendor && bodyData.email) {
       try {
-        const bufferData = new Uint8Array(Buffer.from(req.file.buffer));
+        const bufferData = new Uint8Array(Buffer.from(buffer));
 
         PDF(bufferData).then((data) => {
           const _data = regexMatcher(data, bodyData.vendor);
-          console.log(_data);
+          // console.log(_data.invoicePayment);
+          const parsedInvoicePayment = parseInt(
+            _data.invoicePayment.replace(".", "").replace(",", ".") //This is UGLY. UPGRADE
+          );
           XlsxPopulate.fromFileAsync("./rendicion.xlsx").then((workbook) => {
             // Modify the workbook.
             //DRY
@@ -29,12 +34,22 @@ class InvoiceController {
               .sheet("Detalle_Gastos")
               .cell("E8")
               .value(_data.invoicePayment);
+            if (parsedInvoicePayment <= 2000) {
+              //This is UGLY. UPGRADE
+              workbook
+                .sheet("Detalle_Gastos")
+                .cell("E8")
+                .value(_data.invoicePayment);
+            } else {
+              workbook.sheet("Detalle_Gastos").cell("E8").value(2000);
+            }
             workbook.sheet("Detalle_Gastos").cell("C8").value(bodyData.vendor);
             return workbook.toFileAsync("./out.xlsx");
           });
           res.sendStatus(200);
         });
       } catch (error) {
+        console.log(error);
         res.sendStatus(500);
       }
     } else {
